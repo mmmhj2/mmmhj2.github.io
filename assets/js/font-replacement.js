3d75844fd0bee291b666d23717768de268805133
Lee <li20020410@126.com>
2024-10-07 16:43:10 +0800

    Replace chinese strong tags

--- assets/js/font-replacement.js
+++ assets/js/font-replacement.js
@@ -1,13 +1,19 @@
 
-function replace_italics() {
+function replace_emstrong() {
     console.log("Replacing chinese fonts")
-    const regex_chinese = /\p{Script=Han}+/u
+    const regex_chinese = /\p{Script=Han}+/ug
 
     const em_list = document.querySelectorAll('em')
-    console.log("Found", em_list.length, "<em> tags")
+    console.log("Found", em_list.length, "<em> tags.")
     for (item of em_list) {
-        item.innerHTML = item.innerHTML.replace(regex_chinese, '<citalics>$&</citalics>')
+        item.innerHTML = item.innerHTML.replaceAll(regex_chinese, '<cem>$&</cem>')
+    }
+
+    const strong_list = document.querySelectorAll('strong')
+    console.log("Found", strong_list.length, "<strong> tags.")
+    for (item of strong_list) {
+        item.innerHTML = item.innerHTML.replaceAll(regex_chinese, '<cstrong>$&</cstrong>')
     }
 }
 
-document.addEventListener('DOMContentLoaded', replace_italics, false);
+document.addEventListener('DOMContentLoaded', replace_emstrong, false);
